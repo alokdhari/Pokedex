62d954f4412abbc9b72c93f133d1060528b504ad
Alok Singh <alokdhari@gmail.com>
2021-04-18 17:02:23 +0100

    Skeleton for the api

--- Pokedex.Api/Pokedex.Api/Controllers/PokemonController.cs
+++ Pokedex.Api/Pokedex.Api/Controllers/PokemonController.cs
@@ -1,12 +1,65 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace Pokedex.Api.Controllers
 {
+
     /// <summary>
     /// Defines the <see cref="PokemonController" />.
     /// </summary>
+    [Route("[controller]")]
+    [ApiController]
     public class PokemonController : ControllerBase
     {
+        /// <summary>
+        /// Get details of a <see cref="Pokemon"/> using it's name
+        /// </summary>
+        /// <param name="pokemonName">The pokemon's name<see cref="string"/></param>
+        /// <returns>The <see cref="IActionResult"/>.</returns>
+        [HttpGet]
+        [Route("{pokemonName}")]
+        [ProducesResponseType(typeof(Pokemon), StatusCodes.Status200OK)]
+        public IActionResult GetPokemon(string pokemonName)
+        {
+            try
+            {
+                var pokemon = new Pokemon
+                {
+                    Name = pokemonName
+                };
 
+                return Ok(pokemon);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        /// <summary>
+        /// Get translated details of a <see cref="Pokemon"/> details using it's name
+        /// </summary>
+        /// <param name="pokemonName">The Pokemon's name<see cref="string"/>.</param>
+        /// <returns>The <see cref="IActionResult"/>.</returns>
+        [HttpGet]
+        [Route("translated/{pokemonName}")]
+        [ProducesResponseType(typeof(Pokemon), StatusCodes.Status200OK)]
+        public IActionResult GetTranslatedPokemon(string pokemonName)
+        {
+            try
+            {
+                var pokemon = new Pokemon
+                {
+                    Name = pokemonName
+                };
+
+                return Ok(pokemon);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
     }
 }
---
+++ Pokedex.Api/Pokedex.Api/Models/Pokemon.cs
@@ -0,0 +1,28 @@
+﻿namespace Pokedex.Api.Controllers
+{
+    /// <summary>
+    /// Defines the <see cref="Pokemon" />.
+    /// </summary>
+    public class Pokemon
+    {
+        /// <summary>
+        /// Gets or sets the Habitat.
+        /// </summary>
+        public string Habitat { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether Is_Legendary.
+        /// </summary>
+        public bool IsLegendary { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the StandardDescription.
+        /// </summary>
+        public string StandardDescription { get; set; }
+    }
+}
